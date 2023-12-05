00bc464875345196c30eb49acf329eb50df23b35
Nitesh Singh <69296248+ntssingh@users.noreply.github.com>
2023-12-05 17:04:31 +0530

    Update routes.js

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -2,7 +2,7 @@ import SignPage from "../Pages/SignPage";
 import ActivationPage from "../Pages/ActivationPage";
 import TablePage from "../Pages/TablePage";
 import AnalyticsPage from "../Pages/AnalyticsPage";
-import ErrorPagefrom "../Pages/ErrorPage";
+import ErrorPage from "../Pages/ErrorPage";
 
 export{
 SignPage,
